3bcc7cf8975ae5145078d055c4f2126386fa43d3
Xavier Rousset <xrousset78800@gmail.com>
2017-08-17 15:59:24 +0200

    Ouuuuuuyeaaaahhhhhh le gulp git add gulpfile.js! keurkeur

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 
+
+
+
 gulp.task('sass', function(done) {
 	gulp.src('scss/styles.sass')
 		.pipe(sass())
@@ -9,8 +12,9 @@ gulp.task('sass', function(done) {
 		.pipe(gulp.dest('css/'))
 		.on('end', done);
 });
-// Tâche "watch" = je surveille *less
-gulp.task('watch', ['sass', 'browser-sync'], function() {
+
+// Tâche "watch" = je surveille *less ---> Accessible via "gulp" (tout court)
+gulp.task('default', ['sass', 'browser-sync'], function() {
 	gulp.watch('/css/*.css', ['sass']);
 });
 
